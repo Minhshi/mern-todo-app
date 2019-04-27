4427406b8571111a4861829a6172c89260c50634
Minh Pham <mpham@naver.com>
2019-04-27 12:06:49 +0200

    Add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../pencil.svg";
 import TodoList from "./TodoList";
 import TodoNew from "./TodoNew";
 import TodoEdit from "./TodoEdit";
@@ -10,11 +11,32 @@ class App extends React.Component {
     return (
       <Router>
         <div className="container">
-          <h4>Todo App</h4>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#" target="_blank">
+              <img src={logo} alt="logo" width="30" height="30" />
+            </a>
+            <Link to="/" className="navbar-brand">
+              Todo App
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todo List
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/new" className="nav-link">
+                    Todo New
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route exact path="/" component={TodoList} />
+          <Route exact path="/new" component={TodoNew} />
+          <Route exact path="/edit/:id" component={TodoEdit} />
         </div>
-        <Route exact path="/" component={TodoList} />
-        <Route exact path="/new" component={TodoNew} />
-        <Route exact path="/edit/:id" component={TodoEdit} />
       </Router>
     );
   }
