a2add633683694753b9e866db2cdfa40225d1075
Minh Pham <mpham@naver.com>
2019-04-27 11:31:12 +0200

    Add bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div>App</div>
-      )
+      <div className="container">
+        <h4>Todo App</h4>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
