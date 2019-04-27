88d4734b28143d1d41b5868e7c5346812f2c7ed2
Minh Pham <mpham@naver.com>
2019-04-27 11:40:39 +0200

    Add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <h4>Todo App</h4>
-      </div>
+      <Router>
+        <div className="container">
+          <h4>Todo App</h4>
+        </div>
+        <Route exact path="/" component={TodoList} />
+        <Route exact path="/edit/:id" component={TodoEdit} />
+        <Route exact path="/new" component={TodoNew} />
+      </Router>
     );
   }
 }
